7a73f474c355afa0a4893653117e8e0990ca66bf
William Santos <williamdsw@outlook.com>
2019-10-15 16:51:35 -0300

    Obtendo os dados do formulário de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { CredenciaisDTO } from './../../models/credenciais.dto';
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
@@ -9,27 +10,36 @@ import { MenuController } from 'ionic-angular/components/app/menu-controller';
 })
 export class HomePage 
 {
+  // Instancia de um DTO vazio
+  credenciaisDto : CredenciaisDTO = 
+  {
+    email: "",
+    senha: ""
+  };
+
   // Injetando objeto no construtor da classe
-  constructor (public navCtrl: NavController, public menuControler : MenuController) 
+  constructor (public navCtrl: NavController, public menuControler: MenuController) 
   {
 
   }
 
   // Declaracao de metodo public
-  public login ()
+  public login () 
   {
+    console.log (this.credenciaisDto);
+
     // Todo elemento tem que ser acessado pelo 'this'
     this.navCtrl.setRoot ('CategoriasPage');
   }
 
   // Quando a pagina entrar
-  ionViewWillEnter ()
+  ionViewWillEnter () 
   {
     this.menuControler.swipeEnable (false);
   }
 
   // Quando a pagina sair
-  ionViewDidLeave ()
+  ionViewDidLeave () 
   {
     this.menuControler.swipeEnable (true);
   }
