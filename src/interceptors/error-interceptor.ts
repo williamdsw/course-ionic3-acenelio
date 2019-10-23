47ba1ef2efd86ce8c901e2e645dd76eff64a780a
William Santos <williamdsw@outlook.com>
2019-10-23 12:00:15 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { StorageService } from './../services/storage.service';
 import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
+import { AlertController } from 'ionic-angular';
 
 @Injectable ()
 export class ErrorInterceptor implements HttpInterceptor
 {
-    constructor (public storageService : StorageService)
+    constructor (public storageService : StorageService, public alertControler : AlertController)
     {}
 
     intercept (request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> 
@@ -31,22 +32,79 @@ export class ErrorInterceptor implements HttpInterceptor
 
             switch (objError.status)
             {
+                case 401:
+                {
+                    this.handle401 ();
+                    break;
+                }
+
                 case 403:
                 {
                     this.handle403 ();
                     break;
                 }
 
-                default: {}
+                case 404:
+                {
+                    this.handle404 ();
+                    break;
+                }
+
+                default: 
+                {
+                    this.handleDefaultError (objError);
+                    break;
+                }
             }
 
             return Observable.throw (objError);
         }) as any;
     }
 
+    handle401 ()
+    {
+        // Configuracoes do alert
+        let alert = this.alertControler.create 
+        ({
+            title: "Erro 401: Falha de Autenticação",
+            message: "Email ou senha incorretos",
+            enableBackdropDismiss: false,
+            buttons: [{text: "Ok" }]
+        })
+
+        // Apresenta
+        alert.present ();
+    }
+
     handle403 ()
     {
         this.storageService.setLocalUser (null);
+    }
+
+    handle404 ()
+    {
+        let alert = this.alertControler.create
+        ({
+            title: "Erro 404: Página não encontrada",
+            message: "URL não encontrada",
+            enableBackdropDismiss: false,
+            buttons: [{text: "Ok"}]
+        });
+
+        alert.present ();
+    }
+
+    handleDefaultError (objError)
+    {
+        let alert = this.alertControler.create
+        ({
+            title: `Erro ${objError.status}: ${objError.error}`,
+            message: objError.message,
+            enableBackdropDismiss: false,
+            buttons: [{text: "Ok"}]
+        });
+
+        alert.present ();
     }
 }
 
